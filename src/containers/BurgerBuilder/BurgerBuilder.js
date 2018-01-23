00a8d90d5fa032142d8374eaa6fdf74b7a283dc7
Piotr Szablewski <piotr.szablewski@vp.pl>
2018-01-23 18:48:12 +0100

    Burger is displaing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Auxi'
+import Aux from '../../hoc/Auxi';
+import Burger from '../../components/Burger/Burger'
 class BurgerBuilder extends Component{
   render(){
     return(
       <Aux>
-        <div>Burger</div>
+        <Burger/>
         <div>Burger Controlls</div>
       </Aux>
     );
