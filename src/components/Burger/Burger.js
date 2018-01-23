f233e1835ad2c47da5ff930816e8f369df876c0b
Piotr Szablewski <piotr.szablewski@vp.pl>
2018-01-23 21:14:14 +0100

    Burger is geting ingridients from the state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,16 @@ import classes from './Burger.css'
 import PropTypes from 'prop-types';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 const burger = (props) => {
+    const transIngredients = Object.keys(props.ingredients)
+    .map(ingr => {
+        return [...Array(props.ingredients[ ingr ])].map((_, i) => {
+            return <BurgerIngredient key={ ingr + 1 } type={ ingr }/>;
+        })
+    })
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="salad" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            { transIngredients }
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
