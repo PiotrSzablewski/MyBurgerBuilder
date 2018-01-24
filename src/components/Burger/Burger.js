197b1c53e84017ec372a84476109249b52eeafd1
Piotr Szablewski <piotr.szablewski@vp.pl>
2018-01-24 12:11:25 +0100

    checking if the ingredients where added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,17 @@ import classes from './Burger.css'
 import PropTypes from 'prop-types';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 const burger = (props) => {
-    const transIngredients = Object.keys(props.ingredients)
+    let transIngredients = Object.keys(props.ingredients)
     .map(ingr => {
         return [...Array(props.ingredients[ ingr ])].map((_, i) => {
             return <BurgerIngredient key={ ingr + 1 } type={ ingr }/>;
         })
-    })
+    }).reduce( ( prev, next ) =>{
+        return prev.concat(next);
+    }, [] );
+    if(transIngredients.length === 0){
+        transIngredients = <p>Please add some ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
