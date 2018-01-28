98f5b9bb1e93a8b792f9407b74c8a954dce65b8b
Piotr Szablewski <piotr.szablewski@vp.pl>
2018-01-28 14:01:25 +0100

    replacing the MENU with burger menu button for opening the side drawer

--- src/components/Navigation/SideDraw/DrawerToggel/DrawerToggel.js
+++ src/components/Navigation/SideDraw/DrawerToggel/DrawerToggel.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import classes from './DrawerToggel.css'
 const drawerToggel = (props) => (
-    <div onClick={props.clicked}>MENU</div>
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default drawerToggel;
